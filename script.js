f0db57ba863c6db0958b46633a51f383011a0126
Pepa <kazbek12115@gmail.com>
2024-02-25 15:16:48 +0300

    added check error

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 let money = 1000;
 let step = +prompt("enter your step");
-let bid = prompt("color");
-while(money > 0) {
-    let rand = Math.floor(Math.random() * 2);
-    if (rand === 0) {
-        rand = "red";
-    } else {
-        rand = "black";
+let bid = prompt("color: red or black?");
+
+if (isNaN(step)) {
+    alert("Enter correct value pls!");
+} else {
+    while(money > 0) {
+        let rand = Math.floor(Math.random() * 2);
+        if (rand === 0) {
+            rand = "red";
+        } else {
+            rand = "black";
+        }
+        if (bid === rand) {
+            alert("you win", money);
+            money += step;
+        } else {
+            alert("you lose", money);
+            money -= step;
+            step *= 2;
+        }
+        alert(money);
     }
-    if (bid === rand) {
-        alert("you win", money);
-        money += step;
-    } else {
-        alert("you lose", money);
-        money -= step;
-        step *= 2;
-    }
-    alert(money);
 }
 
-alert(`The casino always wins, now you should ${money * -1}. They've already left for you!`)
+alert(`The casino always wins, now you should ${money * -1}. They've already left for you!`);
